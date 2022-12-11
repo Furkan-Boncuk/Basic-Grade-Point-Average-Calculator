9a8830d68661559f662fc7454b4709271c45cae9
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2022-12-11 17:52:51 +0300

    Update 4-Taximeter-Program.java

--- 4-Taximeter-Program.java
+++ 4-Taximeter-Program.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double km,openTaximeter=10,totalAmount;
+        double km,perKm=2.20,openTaximeter=10,totalAmount;
 
         System.out.println("   ***Taximeter Program***");
         System.out.print("How Many Kilometers Traveled? : ");
         km = input.nextDouble();
 
-        totalAmount = openTaximeter + km*2.20;
+        totalAmount = openTaximeter + km*perKm;
 
         totalAmount = (totalAmount<20)? 20 : totalAmount;
 
